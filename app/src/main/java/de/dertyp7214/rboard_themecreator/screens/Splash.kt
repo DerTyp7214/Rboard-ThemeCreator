53daa923a44819adebba2f346eae313519ed2fe9
DerTyp7214 <lengwenath1@gmail.com>
2019-04-15 00:27:03 +0200

    Style Fixes
    Signed-off-by: DerTyp7214 <lengwenath1@gmail.com>

--- app/src/main/java/de/dertyp7214/rboard_themecreator/screens/Splash.kt
+++ app/src/main/java/de/dertyp7214/rboard_themecreator/screens/Splash.kt
@@ -3,12 +3,15 @@ package de.dertyp7214.rboard_themecreator.screens
 import android.Manifest
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import de.dertyp7214.rboard_themecreator.R
+import de.dertyp7214.rboard_themecreator.core.dp
+import de.dertyp7214.rboard_themecreator.core.getDimensions
 import de.dertyp7214.rboard_themecreator.core.getIntent
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -18,6 +21,28 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val width = getDimensions().first - 60.dp(this@Splash)
+
+        progress.background = resources.getDrawable(R.drawable.progress_shape)
+        progress.layoutParams.width = 0
+        progress.requestLayout()
+
+        ValueAnimator.ofFloat(0F, 1F).apply {
+            duration = 100
+            addUpdateListener {
+                progress.elevation = it.animatedValue as Float * 10
+            }
+            start()
+        }
+        ValueAnimator.ofFloat(0F, 1F).apply {
+            duration = 850
+            addUpdateListener {
+                progress.layoutParams.width =
+                    (width * it.animatedValue as Float).toInt()
+                progress.requestLayout()
+            }
+            start()
+        }
         ObjectAnimator.ofFloat(icLauncher, "scaleY", 1F).apply {
             interpolator = OvershootInterpolator()
             duration = 850
